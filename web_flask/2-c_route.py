7749fc97c485ae076d0345a4939c4fa92b69ec3e
josevelez08 <jose-0919@hotmail.com>
2021-05-02 22:38:56 +0000

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,18 +4,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def router():
+    """ return hello HBNB"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb')
 def router1():
+    """ return HBNB"""
     return "HBNB"
+
 
 @app.route('/c/<text>')
 def print_variable(text):
-        var = text.replace("_", " ")
-        return "C is {}".format(var)
+    """return the url's parameters"""
+    var = text.replace("_", " ")
+    return "C {}".format(var)
 
 
 if __name__ == "__main__":
