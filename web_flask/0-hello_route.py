e47d5092f48be7af42eee2d7e507ca5a657227bb
josevelez08 <jose-0919@hotmail.com>
2021-05-02 21:28:44 +0000

    return a page

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+# page return Hello wit flask
 from flask import Flask
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-@app.route('/', app.strict_slashes = False)
+
+@app.route('/')
 def router():
+    """ return hello HBNB"""
     return "Hello HBNB!"
 
 if __name__ == "__main__":
