b579c7aee123184f6c1230ee2fea560c380683f4
josevelez08 <jose-0919@hotmail.com>
2021-05-02 22:58:56 +0000

    Task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -6,15 +6,18 @@ from flask import render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def router():
     """return hello HBNB"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb')
 def router1():
     """ return HBNB"""
     return "HBNB"
+
 
 @app.route('/python/')
 @app.route('/python/<text>')
@@ -23,16 +26,19 @@ def hello(text="is cool"):
     var = text.replace("_", " ")
     return "Python {}".format(var)
 
+
 @app.route('/c/<text>')
 def print_variable(text):
     """return c and parameters"""
-        var = text.replace("_", " ")
-        return "C is {}".format(var)
+    var = text.replace("_", " ")
+    return "C is {}".format(var)
+
 
 @app.route('/number/<int:n>')
 def print_integer(n):
-        """ return only integer"""
-        return "{} is a number".format(n)
+    """ return only integer"""
+    return "{} is a number".format(n)
+
 
 @app.route('/number_template/<int:n>')
 def print_integer_html(n):
